c80251de9a8fcf95d2ed940fcc9a52a32b663d0a
Yajusenpai <1051148569@qq.com>
2023-06-13 17:18:29 -0400

    Modified GUI

--- database_connect.py
+++ database_connect.py
@@ -2,24 +2,39 @@ import sqlite3
 
 
 def search_all_matching(word):
-    with sqlite3.connect("db.sqlite3") as conn:
+    with sqlite3.connect("notebook.sqlite3") as conn:
         cur = conn.cursor()
-        return [i[0] for i in cur.execute("SELECT word FROM dictionary_info_dictionaryentry WHERE word LIKE ? || '%' ",
+        return [i[0] for i in cur.execute("SELECT word FROM words WHERE word LIKE ? || '%' ",
                                           (word,)).fetchall()]
 
 
 def search_word(word):
-    with sqlite3.connect("db.sqlite3") as conn:
+    with sqlite3.connect("notebook.sqlite3") as conn:
         cur = conn.cursor()
         try:
             return \
-                cur.execute("SELECT description FROM dictionary_info_dictionaryentry WHERE word = ?",
+                cur.execute("SELECT description FROM words WHERE word = ?",
                             (word,)).fetchall()[0][0]
         except IndexError:
             return []
 
 
 def get_all_words():
-    with sqlite3.connect("db.sqlite3") as conn:
+    with sqlite3.connect("notebook.sqlite3") as conn:
         cur = conn.cursor()
-        return [i[0] for i in cur.execute("SELECT word FROM dictionary_info_dictionaryentry").fetchall()]
+        return [i[0] for i in cur.execute("SELECT word FROM words").fetchall()]
+
+
+def insert_new_word(*data):
+    with sqlite3.connect("notebook.sqlite3") as conn:
+        cur = conn.cursor()
+        cur.executemany("INSERT INTO words VALUES(?, ?, ?, ?, ?, ?)", *data)
+        conn.commit()
+
+
+def db_init():
+    with sqlite3.connect("notebook.sqlite3") as conn:
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE words(word, parts_of_speech, meaning, meaning_similar, form_similar,"
+                    "pronoun_similar)")
+        conn.commit()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from database_connect import search_all_matching, search_word, get_all_words
+from database_connect import *
 from tkinter import IntVar
 import pyglet
 import os
@@ -13,15 +13,18 @@ gothic = pyglet.font.load('Century Gothic')
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
+# db_init()
+
+
 class Application(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-        theme_color = "#0b869c"
-        self.geometry("800x500+300+50")
+        theme_color = "#000000"
+        self.geometry("800x600+150+50")
         self.title("English Dictionary")
         # self.overrideredirect(True)
         # self.attributes("-fullscreen", True)
-        self.minsize(800, 550)
+        self.minsize(640, 400)
         # self.iconbitmap("images/cch_logo.ico")
 
         container = tk.Frame(self)
@@ -38,10 +41,10 @@ class Application(tk.Tk):
         self.showFrame(StartPage)
 
         # Frame for the main window bottom credit bar
-        credit_frame = tk.Frame(self, bg=theme_color, relief='groove', bd=0)
+        credit_frame = tk.Frame(self, bg=theme_color, relief='flat', bd=0)
         credit_frame.pack(fill='x')
 
-        label = tk.Label(credit_frame, bg=theme_color, fg='#b5e6e0', text='Adidas Tri Poloski',
+        label = tk.Label(credit_frame, bg=theme_color, fg='#ffffff', text='Adidas Tri Poloski',
                          font=('Times New Roman', 12, 'italic'))
         label.pack(anchor='center')
 
@@ -53,16 +56,16 @@ class Application(tk.Tk):
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        theme_color = "#0b869c"
+        theme_color = "#000000"
         self.config(bg=theme_color)
-        font = ("Century Gothic", 22)
+        font = ("Times New Roman", 26)
         # Frame for main title label and logo
-        mainlabel_frame = tk.Frame(self, bg=theme_color, relief='groove', bd=1)
+        mainlabel_frame = tk.Frame(self, bg=theme_color, relief='flat', bd=1)
         mainlabel_frame.pack(anchor=tk.N)
 
-        title_label = tk.Label(mainlabel_frame, text="An English Dictionary", font=font,
+        title_label = tk.Label(mainlabel_frame, text="Un Dictionnaire Anglais", font=font,
                                bg=theme_color,
-                               fg='#b5e6e0')
+                               fg='#ffffff')
         title_label.pack()
 
         def search(_=None):
@@ -75,6 +78,20 @@ class StartPage(tk.Frame):
                 defination_box.insert(2.0, "_" * 30 + "\n")
                 defination_box.insert(4.0, search_word(typed_word))
                 defination_box.config(state=tk.DISABLED)
+
+        def insert_new(_=None):
+            typed_word = search_entry.get()
+            typed_word = typed_word.split(',')
+            insert_new_word(typed_word)
+
+            # if dictionary_var.get() == 1 and len(typed_word) > 1:
+            #     defination_box.config(state=tk.NORMAL)
+            #     search_entry.delete(0, "end")
+            #     defination_box.delete(1.0, tk.END)
+            #     defination_box.insert(1.0, typed_word.capitalize() + "\n")
+            #     defination_box.insert(2.0, "_" * 30 + "\n")
+            #     defination_box.insert(4.0, search_word(typed_word))
+            #     defination_box.config(state=tk.DISABLED)
 
         def update(data_list):
             # list_box.delete(0, tk.END)
@@ -98,6 +115,13 @@ class StartPage(tk.Frame):
             what_selected(typed)
             # self.update()
 
+        def check_insert(_=None):
+            # grab what is typed
+            typed = insert_entry.get()
+            insert_new_word(typed)
+            # what_selected(typed)
+            # self.update()
+
         def fill_out(_=None):
             # Delete whatever is in the entry box
             search_entry.delete(0, tk.END)
@@ -109,8 +133,8 @@ class StartPage(tk.Frame):
         search_widget_frame = tk.Frame(self, bg=theme_color)
         search_widget_frame.pack(anchor=tk.N)
 
-        def check_me():
-            pass
+        insert_widget_frame = tk.Frame(self, bg=theme_color)
+        insert_widget_frame.pack(anchor=tk.S)
 
         def cut_text():
             copy_text()
@@ -151,33 +175,49 @@ class StartPage(tk.Frame):
         def show_popup(event):
             menu.post(event.x_root, event.y_root)
 
-        checkbuttons_frame = tk.Frame(search_widget_frame)
-        checkbuttons_frame.pack(side=tk.LEFT)
+        # checkbuttons_frame = tk.Frame(search_widget_frame)
+        # checkbuttons_frame.pack(side=tk.LEFT)
 
         images = os.path.join(BASE_DIR, 'img')  # images directory path
         on_image = tk.PhotoImage(file=os.path.join(images, 'button_on.png'))
         off_image = tk.PhotoImage(file=os.path.join(images, 'button_off.png'))
         dictionary_var = IntVar()
-        dictionary_check = tk.Checkbutton(search_widget_frame, image=off_image, selectimage=on_image, indicatoron=False,
-                                          variable=dictionary_var, onvalue=1, offvalue=0, bd=0,
-                                          text="Dictionary", bg=theme_color, command=check_me)
-        dictionary_check.image = (on_image, off_image)
-        dictionary_check.pack(side=tk.LEFT)
-        dictionary_check.select()
+        # dictionary_check = tk.Checkbutton(search_widget_frame, image=off_image, selectimage=on_image, indicatoron=False,
+        #                                   variable=dictionary_var, onvalue=1, offvalue=0, bd=0,
+        #                                   text="Dictionary", bg="#ffffff", command=check_me)
+        # dictionary_check.image = (on_image, off_image)
+        # dictionary_check.pack(side=tk.LEFT)
+        # dictionary_check.select()
 
-        search_text = tk.Label(search_widget_frame, bg=theme_color, text="Search ", font=("Century Gothic", 14))
+        search_text = tk.Label(search_widget_frame, bg="#ffffff", text="Search ", font=("Times New Roman", 14))
         search_text.pack(side=tk.LEFT)
 
-        search_entry = tk.Entry(search_widget_frame, width=34, font=("Century Gothic", 18))
-        search_entry.pack(pady=20, side=tk.LEFT)
+        search_entry = tk.Entry(search_widget_frame, width=34, font=("Times New Roman", 18))
+        search_entry.pack(pady=20, side=tk.RIGHT)
         search_entry.focus_set()
         search_entry.bind("<KeyRelease>", check)
 
         search_img = tk.PhotoImage(file=os.path.join(images, 'search.png'))
         search_btn = tk.Button(search_widget_frame, image=search_img, bg=theme_color, width=80,
-                               activebackground='#5e5924', command=search)
+                               activebackground='#6b5924', command=search)
         search_btn.image = search_img
-        search_btn.pack(side=tk.LEFT, padx=5)
+        search_btn.pack(side=tk.RIGHT, padx=5)
+
+        # below are insert new word related
+
+        insert_text = tk.Label(insert_widget_frame, bg="#ffffff", text="New Word", font=("Times New Roman", 14))
+        insert_text.pack(side=tk.LEFT)
+
+        insert_entry = tk.Entry(insert_widget_frame, width=34, font=("Times New Roman", 18))
+        insert_entry.pack(pady=20, side=tk.RIGHT)
+        insert_entry.focus_set()
+        insert_entry.bind("<KeyRelease>", check_insert)
+
+        insert_img = tk.PhotoImage(file=os.path.join(images, 'search.png'))
+        insert_btn = tk.Button(insert_widget_frame, image=insert_img, bg=theme_color, width=80,
+                               activebackground='#6b5924', command=insert_new)
+        insert_btn.image = search_img
+        insert_btn.pack(side=tk.LEFT, padx=5)
 
         defination_frame = tk.Frame(self)
         defination_frame.pack(fill=tk.BOTH, padx=10, expand=True)
